5c80982b9692d9411a8f57e22ad4144839610c1b
mimondesu <65010914603@msu.ac.th>
2025-03-04 15:59:05 +0700

    Update connectdb.php

--- connectdb.php
+++ connectdb.php
@@ -2,8 +2,8 @@
 
 $host = "localhost" ;
 $user = "root";
-$pwd = "";
-$db = "shop";
+$pwd = "147852369";
+$db = "shop4603";
 $conn = mysqli_connect($host,$user,$pwd) or die ("เชื่อมต่อฐานข้อมูลไม่ได้");
 mysqli_select_db($conn,$db) or die ("เลือกฐานข้อมูลไม่ได้");
 mysqli_query($conn, "set names utf8");
